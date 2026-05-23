56109bfa7e9292650b4014da748cfb5eab416514
Nakata Modem <nakatamodem@hotmail.com>
2026-05-23 10:03:11 +0200

    shift from db approach to file logs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
 use anyhow::Result;
-use sqlx::SqlitePool;
+// use sqlx::SqlitePool;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // connect to the database
-    let pool = SqlitePool::connect("sqlite://scanner.db").await?;
+    // connect to the database // `$ touch scanner.db`
+    // let pool = SqlitePool::connect("sqlite://scanner.db").await?;
 
     // execute the schema creation
-    sqlx::query!(
-        "CREATE TABLE IF NOT EXISTS scans (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            ip TEXT NULL,
-            port INTEGER NULL NULL,
-            status TEXT NOT NULL,
-            scanned_at DATETIME DEFAULT CURRENT_TIMESTAMP
-        )"
-    )
-    .execute(&pool)
-    .await?;
-
-    println!("Hello, world!");
+    // sqlx::query!(
+    //     "CREATE TABLE IF NOT EXISTS scans (
+    //         id INTEGER PRIMARY KEY AUTOINCREMENT,
+    //         ip TEXT NULL,
+    //         port INTEGER NULL NULL,
+    //         status TEXT NOT NULL,
+    //         scanned_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    //         url TEXT NOT NULL
+    //     )"
+    // )
+    // .execute(&pool)
+    // .await?;
 
     Ok(())
 }
