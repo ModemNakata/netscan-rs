a43698779fa637d07f43a9bce4fe2ea68c721dcc
Nakata Modem <nakatamodem@hotmail.com>
2026-05-23 09:11:18 +0200

    database connection + schema

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
+use anyhow::Result;
+use sqlx::SqlitePool;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // connect to the database
+    let pool = SqlitePool::connect("sqlite://scanner.db").await?;
+
+    // execute the schema creation
+    sqlx::query!(
+        "CREATE TABLE IF NOT EXISTS scans (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ip TEXT NULL,
+            port INTEGER NULL NULL,
+            status TEXT NOT NULL,
+            scanned_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        )"
+    )
+    .execute(&pool)
+    .await?;
+
     println!("Hello, world!");
+
+    Ok(())
 }
